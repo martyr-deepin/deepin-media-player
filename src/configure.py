e6f36e55a00be328317741bfd7de4f911f7d82b6
hailongqiu <qiuhailong@linuxdeepin.com>
2012-03-31 14:23:25 +0800

    modify configure.py

--- src/configure.py
+++ src/configure.py
@@ -26,12 +26,15 @@ import ConfigParser
 import string, os, sys
 import gobject
 
+from utils import *
+
+
 class MediaConfig(gobject.GObject):
     __gsignals__ = {
         "media-config":(gobject.SIGNAL_RUN_LAST,
-                        gobject.TYPE_NONE,()),
-        "media-modify":(gobject.SIGNAL_RUN_LAST,
                         gobject.TYPE_NONE,())
+        # "media-modify":(gobject.SIGNAL_RUN_LAST,
+        #                 gobject.TYPE_NONE,())
         }
     def __init__(self):
         gobject.GObject.__init__(self)
@@ -44,67 +47,78 @@ class MediaConfig(gobject.GObject):
         self.cf.read(config_path)    
         self.emit("media-config")        
         
-    def get(self, section, option):    
-        return self.cf.get(section, option)
-        
+    def get_value(self, section, option):    
+        try:
+            return self.cf.get(section, option)
+        except:
+            return -1
+            
     def get_options(self, section):
-        return self.cf.options(section)
+        try:
+            return self.cf.options(section)
+        except:
+            return -1
     
     def get_items(self, section):
-        
-        return self.cf.items(section)
+        try:
+            return self.cf.items(section)
+        except:
+            return -1
         
     def get_sections(self):
-        return self.cf.sections()
-    
+        try:
+            return self.cf.sections()
+        except:
+            return -1
+            
     def add_section(self, section):
         self.cf.add_section(section)
         
     def set_value(self, section, option, value):    
         self.cf.set(section, option , value)
         
-    def write(self):    
+    def save_config(self):    
         fp = open(self.config_path, "w")
         self.cf.write(fp)
         fp.close()
         
-    def FSMonitor(self):
-        if "" != self.config_path:
-            wm = WatchManager() 
-            mask = IN_MODIFY
-            notifier = Notifier(wm, self.process_IN_MODIFY)
-            wm.add_watch(self.config_path, mask, rec=True)
-            print 'now starting monitor %s'%(self.config_path)
-            while True:
-                try:
-                    notifier.process_events()
-                    if notifier.check_events():
-                        notifier.read_events()
-                except KeyboardInterrupt:
-                    notifier.stop()
-                    break
+    # def FSMonitor(self):
+    #     if "" != self.config_path:
+    #         wm = WatchManager() 
+    #         mask = IN_MODIFY
+    #         notifier = Notifier(wm, self.process_IN_MODIFY)
+    #         wm.add_watch(self.config_path, mask, rec=True)
+    #         print 'now starting monitor %s'%(self.config_path)
+    #         while True:
+    #             try:
+    #                 notifier.process_events()
+    #                 if notifier.check_events():
+    #                     notifier.read_events()
+    #             except KeyboardInterrupt:
+    #                 notifier.stop()
+    #                 break
     
-    def process_IN_MODIFY(self, event):
-        '''File modify emit signal.'''
-        print   "Modify file: %s "  %   os.path.join(event.path, event.name)
-        self.emit("media-modify")
+    # def process_IN_MODIFY(self, event):
+    #     '''File modify emit signal.'''
+    #     print   "Modify file: %s "  %   os.path.join(event.path, event.name)
+    #     self.emit("media-modify")
         
 def Test(config):
     print "fdsf"
         
 if __name__ == "__main__":
     mc = MediaConfig()        
-    mc.connect("media-config", Test)
-    mc.init_read("/home/long/.config/deepin-media-player/config.ini")
+    #mc.connect("media-config", Test)
+    mc.init_read(get_home_path() + "/.config/deepin-media-player/config.ini")
     # mc.add_section("window_mode")
     # mc.set_value("window_mode", "ss", "50")
     # mc.set_value("window_mode", "vv", "150")
-    print mc.get_options("window_mode")
+    print mc.get_options("window")
     print mc.get_items("window_mode")
     print mc.get_sections()
     
-    print mc.get_items("window_mode")
-    print mc.get("window_mode", "ss")
+    print mc.get_items("window")
+    print mc.get_value("window_mode", "ss")
     #mc.write()
     #mc.FSMonitor()
 
