b30b34f891dbcfecad629f376262c70bd748d28b
hailongqiu <qiuhailong@linuxdeepin.com>
2012-05-15 10:48:37 +0800

    mplayer. BUG: get_lenght modify code.

--- src/player_box.py
+++ src/player_box.py
@@ -419,9 +419,8 @@ class PlayerBox(object):
         
     def length_threads(self):
         '''Get length threads'''
-        for i in self.play_list.list_view.items:
-            
-            i.length = str(get_length(self.play_list_dict[i.title]))
+        for i in self.play_list.list_view.items:             
+            i.length, length = get_length(self.play_list_dict[i.title])
             i.emit_redraw_request()
             
             
