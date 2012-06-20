116ab1c0131e1cf2de18b2aaa1015ee7021811af
long <long@long-U31Jg.(none)>
2012-06-20 17:13:34 +0800

    add image. modify code.

--- src/toolbar.py
+++ src/toolbar.py
@@ -39,7 +39,7 @@ class ToolBar(object):
         self.show = 0
         self.hbox = gtk.HBox()
         
-        self.panel = Panel(APP_WIDTH - 5, PANEL_HEIGHT, window_type=gtk.WINDOW_POPUP)        
+        self.panel = Panel(APP_WIDTH - 5, PANEL_HEIGHT + 5, window_type=gtk.WINDOW_POPUP)        
         self.panel.connect("enter-notify-event", self.show_panel_toolbar)
         self.panel.connect("leave-notify-event", self.hide_panel_toolbar)
         self.panel.connect("focus-out-event", self.focus_hide_toolbar)
