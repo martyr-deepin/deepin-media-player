b23b95b2271e1b5738f896294ed8a4fb1fe316b3
Andy Stewart <lazycat.manatee@gmail.com>
2012-08-14 12:22:36 +0800

    Refactory skin module with newest deepin-ui code.

--- src/skin.py
+++ src/skin.py
@@ -1,28 +1,34 @@
-from dtk.ui.theme         import Theme, ui_theme
-from dtk.ui.skin_config   import skin_config
-from dtk.ui.utils         import get_parent_dir
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2011 ~ 2012 Deepin, Inc.
+#               2011 ~ 2012 Wang Yong
+# 
+# Author:     Wang Yong <lazycat.manatee@gmail.com>
+# Maintainer: Wang Yong <lazycat.manatee@gmail.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from constant import PROGRAM_NAME, PROGRAM_VERSION
-
+from dtk.ui.init_skin import init_skin
+from dtk.ui.utils import get_parent_dir
 import os
 
-
-
-# Init skin config.
-skin_config.init_skin(
+app_theme = init_skin(
+    PROGRAM_NAME,
+    PROGRAM_VERSION,
     "blue",
     os.path.join(get_parent_dir(__file__, 2), "skin"),
-    os.path.expanduser("~/.config/deepin-media-player/skin"),
-    os.path.expanduser("~/.config/deepin-media-player/skin_config.ini"),
-    PROGRAM_NAME,
-    PROGRAM_VERSION
-    )
-
-# Create application theme.
-app_theme = Theme(
     os.path.join(get_parent_dir(__file__, 2), "app_theme"),
-    os.path.expanduser("~/.config/deepin-media-player/theme")
     )
-
-# Set theme.
-skin_config.load_themes(ui_theme, app_theme)
