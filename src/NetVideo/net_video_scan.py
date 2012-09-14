f5c2fb5eb3962ec27c265643f3b8f3e58d75b4ca
long <long@long-storm.(none)>
2012-09-14 17:04:25 +0800

    netvideo get image.

--- src/NetVideo/net_video_scan.py
+++ src/NetVideo/net_video_scan.py
@@ -20,6 +20,124 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
+from dtk.ui.utils import is_network_connected
+import re
+import socket
+import urllib2
+import urllib
+import threading
+
+# Error Code.
+ERROR_CODE_NETWORK_CONNECT_LOSE     = -1 # 网络链接失败.
+ERROR_CODE_SCAN_LOSE                = -2 # 搜索失败.
+
+
+FLVCD_TO_PLAY_HTML = 'http://www.flvcd.com/parse.php?flag=&format=&kw='
+YOUKU_SCAN_HTML = 'http://www.soku.com/search_video/q_'
+# TUDOU_SCAN_HTML = 'http://www.soku.com/t/nisearch/'
+# PPAS_SCAN_HTML = ''
+
+class ScanThreads(threading.Thread):
+    '''线程池'''    
+    def __init__(self, threads_queue):
+        threading.Thread.__init__(self)        
+        self.threads_queue = threads_queue
+        self.threads_lsit = []
+        pass
+    
+    def run(self):
+        pass
+    
+    def start(self):
+        pass
+    
+    
+    
+class NetVideoScan(object):        
+    def __init__(self):
+        self.error_code = None
+        self.scan_sum = 0
+            
+    def scan(self, scan_name_key, scan_engine="youku", seconds=100):
+        if is_network_connected(): # Test networ connect.
+            if scan_engine == "youku":
+                return_info = self.scan_youku_function(scan_name_key, seconds)
+            return return_info
+        else:
+            self.error_code = ERROR_CODE_NETWORK_CONNECT_LOSE
+            return False
+        
+    ###################################################################    
+    ### youku scan engine.
+    def scan_youku_function(self, scan_name_key, seconds):
+        try:
+            scan_name_key = urllib.quote(scan_name_key)
+            scan_html = YOUKU_SCAN_HTML + scan_name_key
+            # Open url.
+            url_open = urllib2.urlopen(scan_html)
+            patter_string =  url_open.read()
+            # Get scan sum.
+            self.scan_youku_sum(patter_string)
+            # Get page url.
+            self.scan_page_url(patter_string)
+            return True
+        except Exception, e:
+            print "scan_youku_function[Error]:", e
+    
+    def scan_youku_sum(self, patter_string):
+        patter = r'共找到.+个结果'
+        findall_return_list = self.scan_findall(patter, patter_string)
+            
+        if len(findall_return_list) <= 0:
+            self.error_code = ERROR_CODE_SCAN_LOSE
+            self.scan_sum   = 0
+            return False
+        else:
+            self.scan_sum = findall_return_list[0].strip()
+        
+    def scan_page_url(self, patter_string):
+        # patter = r"<li class=\"v_link\">.+"
+        # findall_return_list = self.scan_findall(patter, patter_string)
+        # for i in findall_return_list:
+            # print i
+        
+        # Get url adder.
+        # Get image.
+        patter = r'<img alt=.+>'
+        findall_return_list = self.scan_findall(patter, patter_string)
+        image_patter = r'src=\"(.+)\" '
+        name_patter = r'alt=\"(.+)\" src='
+        i = 0
+        for findall_text in findall_return_list:
+            scan_image = self.scan_findall(image_patter, findall_text)[0]
+            name_image = self.scan_findall(name_patter, findall_text)[0]
+            url_image = urllib2.urlopen(scan_image)
+            fp = open("/tmp/%s.jpg"%(name_image), "w")
+            fp.write(url_image.read())
+            fp.close()
+            i += 1
+            print "name:",  name_image
+            print "image:", scan_image
+        pass
+    
+    def scan_findall(self, patter, patter_string):
+        # fp = open("123.html", "w")
+        # fp.write(patter_string)
+        # fp.close()
+        return re.findall(patter, patter_string)
+        
+if __name__ == "__main__":
+    net_video_scan = NetVideoScan()
+    if not net_video_scan.scan("编译原理"):
+        if net_video_scan.error_code == ERROR_CODE_NETWORK_CONNECT_LOSE:
+            print "网络链接失败"
+        elif net_video_scan.error_code == ERROR_CODE_SCAN_LOSE:    
+            print "搜索不到.."
+    else:
+        print "搜索成功"
+            
+
 ''' [支持视频网站]
 优酷网	土豆网	奇艺网	搜狐视频
 酷6网	腾讯视频	PPTV	新浪播客
@@ -51,83 +169,5 @@ ACFUN	嗶哩嗶哩	看看新闻网	酷米网
 国外视频网站(6)：
 YouTube	CollegeHumor	WAT TV	ESL TV
 5min	Howcast
+当我写视频播放器的视频,我已经看见我离监狱越来越近了,^_^..哈哈哈..搞起!!爬.
 '''
-
-import re
-import socket
-import urllib2
-import threading
-
-FLVCD_TO_PLAY_HTML = 'http://www.flvcd.com/parse.php?flag=&format=&kw='
-YOUKU_SCAN_HTML = 'http://www.soku.com/search_video/q_'
-TUDOU_SCAN_HTML = 'http://www.soku.com/t/nisearch/'
-PPAS_SCAN_HTML = ''
-
-class ScanThreads(threading.Thread):
-    def __init__(self, threads_queue):
-        threading.Thread.__init__(self)
-        self.threads_queue = threads_queue
-        self.threads_lsit = []
-        pass
-    
-    def run(self):
-        pass
-    
-    def start(self):
-        pass
-    
-    
-    
-class NetVideoScan(object):
-    def __init__(self):
-        pass
-            
-    def scan(self, scan_name, seconds=100):
-        return self.scan_threading(scan_name, seconds)
-        
-    def scan_threading(self, scan_name, seconds):
-        # socket.setdefaulttimeout(seconds)
-        scan_html = YOUKU_SCAN_HTML + scan_name
-        print "scan_html:", scan_html
-        # scan_html = "http://www.soku.com/search_video/q_编译原理fdsjfklsjdlkf"
-        # scan_html = "http://www.soku.com/t/nisearch/编译原理/"
-        scan_html = "http://www.flvcd.com/parse.php?flag=&format=&kw=http%3A%2F%2Fv.pps.tv%2Fplay_32JV2L.html%23from_www&sbt=%BF%AA%CA%BCGO%21"
-        headers = {
-            "User-Agent":"Opera/9.80 (X11; Linux i686; U; zh-cn) Presto/2.10.289 Version/12.02",
-            "Accept": "text/plain"} # 请求头.
-        req = urllib2.Request(scan_html, headers=headers)
-        url_open = urllib2.urlopen(req)        
-        patter_string =  url_open.read()
-        # 第一步: 查看搜索到多少个.    
-        patter = r'共找到.+个结果'
-        findall_return_list = self.scan_findall(patter, patter_string)
-        # print findall_return_list
-        print patter_string
-        if len(findall_return_list) <= 0:
-            return False
-        else:
-            findall_return_list = findall_return_list[0].strip()
-            # net_video_sum = int(findall_return_list)
-            print "findall_return_list:[视频总数]", findall_return_list
-            
-        # 第二步: 获取 下一页 信息--->> _orderby_1_page_2 _orderby_1_page_3 _orderby_1_page_?
-        return True
-    
-    def scan_findall(self, patter, patter_string):
-        fp = open("123.txt", "w")
-        fp.write(patter_string)
-        fp.close()
-        return re.findall(patter, patter_string)
-        
-if __name__ == "__main__":
-    net_video_scan = NetVideoScan()
-    if not net_video_scan.scan("成龙"):
-        print "搜索不到"
-    else:    
-        print "搜索成功"
-        
-    
-    # path_thread_id = threading.Thread(target=self.scan_threading, args=(scan_name, seconds,))
-    # path_thread_id.setDaemon(True)
-    # path_thread_id.start()
-    
