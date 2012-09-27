06d33aa545efb6d37e210b4f4c58101672bf2090
long <long@long-LD.(none)>
2012-09-27 10:21:47 +0800

    convertorrs add on_message , pipep..

--- src/video_format_conv/convertors.py
+++ src/video_format_conv/convertors.py
@@ -21,14 +21,57 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import gobject
+import gst
 
+NULL  = gst.STATE_NULL
+PAUSE = gst.STATE_PAUSED
+
+# 保存视频格式转化的各个信息
 class Format:
     def __init__(self):
         self.source_file = ""
         self.output_file = ""
         self.codecaps = None
     
+        
 class Convertors(gobject.GObject):
+    __gsignals__ = {
+        'convertors-update-progressbar' : (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, ()),
+        'convertors-eos' : (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, ()),
+        'convertors-error' : (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (gobject.TYPE_PYOBJECT,))
+            }    
     def __init__(self, _format):
+        gobject.GObject.__init__(self)
+        #
         self.__format = _format
+        #
+        self.pipeline = gst.Pipeline("Convertors")
+        self.pipeline.set_state(PAUSE)
+        self.uridecoder = gst.element_make_factor("uridecoder", "uridecoder")
+        self.player_name.set_property("uri", "file://%s" % (self.__format.source_file))
+        #
+        # gst.Caps("audio/mpeg")
+        #
+        self.pipeline.add(self.uridecoder)
+        self.pipeline.set_state(PAUSE)
+        
+        #
+        bus = self.player.get_bug()
+        bus.add_signal_watch()
+        bus.connect("message", self.on_message)
+        
+    def on_message(self, bus, message): 
+        __type = message.type
+        if gst.MESSAGE_EOS == __type: # 结束
+            self.player.set_state(NULL)
+            self.emit("convertors-eos")
+        elif gst.MESSAGE_ERROR == __type: # 错误信息
+            self.player.set_state(NULL)
+            (err, debug) = message.parse_error()
+            self.emit("convertors-error", err.message) # 发送错误信息给GUI界面.
+        elif gst.MESSAGE_ASYNC_DONE == __type: # 更新进度条
+            self.emit("convertors-update-progressbar") # 发送更新信号给GUI界面更新进度条.            
+            
+            
+            
         
